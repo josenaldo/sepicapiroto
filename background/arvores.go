044ab4196c53d51693f7fe5cc83b9c5738c95d43
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2020-07-15 22:50:02 -0300

    Criamos o cenário da montanha e da arvore. Adicionamos propriedades nos scripts.

---
+++ background/arvores.go
@@ -0,0 +1,35 @@
+components {
+  id: "arvores"
+  component: "/background/arvores.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/background/background.atlas\"\n"
+  "default_animation: \"layer-3\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 640.0
+    y: 360.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
---
+++ background/montanhas.go
@@ -0,0 +1,35 @@
+components {
+  id: "montanhas"
+  component: "/background/montanhas.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/background/background.atlas\"\n"
+  "default_animation: \"layer-2\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
